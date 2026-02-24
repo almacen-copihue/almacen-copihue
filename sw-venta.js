2fe5b8d698c2eb17c7a32222874f72d391561895
almacen-copihue <javierojedabariloche@gmail.com>
2026-02-24 20:52:37 -0300

    Add files via upload

--- sw-venta.js
+++ sw-venta.js
@@ -1,5 +1,5 @@
 // ========== SERVICE WORKER — COPIHUE PUNTO DE VENTA ==========
-const VERSION = 'copihue-venta-v8';
+const VERSION = 'copihue-venta-v10';
 const CACHE_ESTATICO = [
     '/venta.html',
     '/manifest-venta.json'
